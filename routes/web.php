ebcb0be479732d64a382f9fa2fa64126161149e8
zzeis <moisesmonteiro.stark@gmail.com>
2024-01-15 10:37:44 -0300

    add route admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\site\SiteController;
+use App\Http\Controllers\Admin\{SupportController};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::get('supports', [SupportController::class, 'index'])->name('supports');
+Route::get('/contato', [SiteController::class, 'contact']);
 Route::get('/', function () {
     return view('welcome');
 });
