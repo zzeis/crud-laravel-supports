42ef610af8a13abbe46f674b13b1a575bd44a97d
zzeis <moisesmonteiro.stark@gmail.com>
2024-01-16 14:21:32 -0300

    new view to the form novas duvidas

---
+++ resources/views/Admin/supports/create.blade.php
@@ -0,0 +1,7 @@
+<h1>Nova duvida</h1>
+
+<form method="POST" action="">
+    <input type="text" placeholder="assunto" name="subject">
+    <textarea name="body" cols="30" rows="5" placeholder="descricao"></textarea>
+    <button type="submit">Enviar</button>
+</form>
