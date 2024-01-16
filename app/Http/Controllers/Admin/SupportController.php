340cb5f0ed6c17e290bc472c0fe9532e410713bc
zzeis <moisesmonteiro.stark@gmail.com>
2024-01-16 14:20:32 -0300

    new metodo create add

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(){
-        return view('Admin/supports/index');
+    public function index(Support $support)
+    {
+
+        $supports = $support->all();
+
+
+        return view('Admin/supports/index', compact('supports'));
+    }
+
+    public function create()
+    {
+        return view('Admin/supports/create');
     }
 }
