6aa3fdbd162a8489dfde19a7a97d9d1316c90b41
zzeis <moisesmonteiro.stark@gmail.com>
2024-01-18 15:14:24 -0300

    add token at form

--- resources/views/Admin/supports/create.blade.php
+++ resources/views/Admin/supports/create.blade.php
@@ -1,6 +1,8 @@
 <h1>Nova duvida</h1>
 
-<form method="POST" action="">
+<form method="POST" action="{{route('supports.store')}}">
+    {{-- <input type="text" name="_token" value="{{ csrf_token()}}"> --}}
+    @csrf()
     <input type="text" placeholder="assunto" name="subject">
     <textarea name="body" cols="30" rows="5" placeholder="descricao"></textarea>
     <button type="submit">Enviar</button>
