720d417d371013592353e51811deb57d2c9a8b9f
zzeis <moisesmonteiro.stark@gmail.com>
2024-01-18 15:14:06 -0300

    new route for the details support

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('supports/{id}',[SupportController::class, 'show'])->name('show');
+Route::post('/supports',[SupportController::class, 'store'])->name('supports.store');
+Route::get('supports/create', [SupportController::class, 'create'])->name('supports.create');
+Route::get('supports', [SupportController::class, 'index'])->name('supports.index');
 
 
-Route::get('supports/create', [SupportController::class, 'create'])->name('create');
-Route::get('supports', [SupportController::class, 'index'])->name('supports');
 Route::get('/contato', [SiteController::class, 'contact']);
 Route::get('/', function () {
     return view('welcome');
