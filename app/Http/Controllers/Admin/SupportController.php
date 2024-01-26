979beb1aea0a2349550cbfefffeac979bdc287c9
zzeis <moisesmonteiro.stark@gmail.com>
2024-01-26 09:58:09 -0300

    add validation errors for the fields

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreSupportRequest;
 use App\Models\Support;
 use Illuminate\Http\Request;
 
@@ -17,17 +18,69 @@ class SupportController extends Controller
         return view('Admin/supports/index', compact('supports'));
     }
 
+    public function show(string|int $id)
+    {
+
+        //$Support::find($id);
+        //Support::where('id', $id)->first();
+        //Suports::where('id','=', $id)->first();
+
+        if (!$support =  Support::find($id)) {
+            return back();
+        }
+
+        return view('Admin/supports/show', compact('support'));
+    }
+
     public function create()
     {
         return view('Admin/supports/create');
     }
 
-    public function store(Request $request, Support $support)
+    public function store(StoreSupportRequest $request, Support $support)
     {
-        $data = $request->all();
+        $data = $request->validated();
         $data['status'] = 'a';
         $support = $support->create($data);
-        
+
         return redirect()->route('supports.index');
+    }
+
+    public function edit(Support $support, string|int $id)
+    {
+        if (!$support = $support->where('id', $id)->first()) {
+            return back();
+        }
+
+        return view('Admin/supports/edit', compact('support'));
+    }
+
+    public function update(StoreSupportRequest $request, Support $support, string|int $id)
+    {
+        if (!$support =  $support->find($id)) {
+            return back();
+        }
+
+
+        //alternative update
+        // $support->subject = $request->subject;
+        // $support->body = $request->body;
+        // $support->save();
+
+        $support->update($request->validated());
+
+        return redirect()->route('supports.index');
+    }
+
+    public function destroy(string|int $id)
+    {
+        if (!$support =  Support::find($id)) {
+            return back();
+        }
+
+        $support->delete();
+
+        return redirect()->route('supports.index');
+
     }
 }
