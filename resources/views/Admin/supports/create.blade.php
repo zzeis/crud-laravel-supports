7c1e438c603da072fbc74c1cee16dc106708c3a7
zzeis <moisesmonteiro.stark@gmail.com>
2024-01-26 09:57:27 -0300

    show erros for the form

--- resources/views/Admin/supports/create.blade.php
+++ resources/views/Admin/supports/create.blade.php
@@ -1,9 +1,14 @@
 <h1>Nova duvida</h1>
 
-<form method="POST" action="{{route('supports.store')}}">
+@if ($errors->any())
+    @foreach ($errors->all() as $error)
+        {{ $error }}
+    @endforeach
+@endif
+<form method="POST" action="{{ route('supports.store') }}">
     {{-- <input type="text" name="_token" value="{{ csrf_token()}}"> --}}
     @csrf()
-    <input type="text" placeholder="assunto" name="subject">
-    <textarea name="body" cols="30" rows="5" placeholder="descricao"></textarea>
+    <input type="text" placeholder="assunto" name="subject" value="{{old('subject')}}">
+    <textarea name="body" cols="30" rows="5" placeholder="descricao">{{old('body')}}</textarea>
     <button type="submit">Enviar</button>
 </form>
