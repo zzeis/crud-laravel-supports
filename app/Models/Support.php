5b702505e08f02c6149fdf46d11d92c51d637b76
zzeis <moisesmonteiro.stark@gmail.com>
2024-03-05 14:32:49 -0300

    methot get api

--- app/Models/Support.php
+++ app/Models/Support.php
@@ -2,16 +2,25 @@
 
 namespace App\Models;
 
+use App\Enums\SupportStatus;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Support extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = [
         'subject',
         'status',
         'body'
     ];
+
+    public function status(): Attribute
+    {
+        return Attribute::make(
+            set: fn (SupportStatus $status) => $status->name,
+        );
+    }
 }
