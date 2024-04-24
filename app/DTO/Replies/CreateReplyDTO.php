7a1c18956e8ff976b201ddf0a54473b412f25dd8
zzeis <moisesmonteiro.stark@gmail.com>
2024-04-24 14:21:21 -0300

    creating DTO Reply

---
+++ app/DTO/Replies/CreateReplyDTO.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\DTO\Replies;
+
+use App\Enums\SupportStatus;
+use App\Http\Requests\StoreReplySupportRequest;
+
+class CreateReplyDTO
+{
+    public function __construct(
+        public string $supportId,
+        public string $content,
+    ) {
+        
+    }
+
+    public static function makeFromRequest(StoreReplySupportRequest $request): self
+    {
+        
+        return new self(
+            $request->support_id,
+            $request->content
+
+        );
+    }
+}
