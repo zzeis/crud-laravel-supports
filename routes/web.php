787624ef52798893b14e6c33488deb0b7f029e8b
zzeis <moisesmonteiro.stark@gmail.com>
2024-04-24 14:19:09 -0300

    add router replies

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Enums\SupportStatus;
+use App\Http\Controllers\site\SiteController;
+use App\Http\Controllers\Admin\{ReplySupportController, SupportController};
 use App\Http\Controllers\ProfileController;
+use App\Models\Support;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +18,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test', function () {
+    dd(array_column(SupportStatus::cases(), 'name'));
+});
+
+
+
+
+Route::get('/contato', [SiteController::class, 'contact']);
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -26,6 +37,22 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::post('/supports/{id}/replies', [ReplySupportController::class, 'store'])->name('replies.store');
+    Route::delete('/supports/{id}/replies/{reply}', [ReplySupportController::class, 'destroy'])->name('replies.destroy');
+    Route::get('/supports/{id}/replies', [ReplySupportController::class, 'index'])->name('replies.index');
+
+
+
+    //Routes supports 
+    Route::delete('supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
+    Route::put('supports/{id}', [SupportController::class, 'update'])->name('supports.update');
+    Route::get('supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
+    Route::get('supports/create', [SupportController::class, 'create'])->name('supports.create');
+    Route::get('supports/{id}', [SupportController::class, 'show'])->name('supports.show');
+    Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
+    Route::get('supports', [SupportController::class, 'index'])->name('supports.index');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
